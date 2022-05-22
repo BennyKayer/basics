57be5a03eb3bc918cb0162ee53ca703ecee26ef3
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-05-22 17:15:06 +0200

    #05.55 Dealing with Numbers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,8 @@ export class AppComponent {
     imgURL =
         'https://i.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0';
     currentDate = new Date();
+    cost = 2000;
+    temperature = 25.3;
 
     getName() {
         return this.name;
