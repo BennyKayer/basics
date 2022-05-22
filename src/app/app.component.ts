ce6dc1b38dc5158df022c75ce5f3208d20885132
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-05-22 17:01:21 +0200

    #05.54 Pipe parameters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
     name = 'luis ramirez';
     imgURL =
         'https://i.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0';
+    currentDate = new Date();
 
     getName() {
         return this.name;
