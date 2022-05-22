ec29c4891413611c569648127c16a317a51473bd
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-05-22 16:06:35 +0200

    #04.49 Exploring More Lifecycle Hooks

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,11 +1,34 @@
-import { Component, Input, EventEmitter, Output, OnInit } from '@angular/core';
+import {
+    Component,
+    Input,
+    EventEmitter,
+    Output,
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewChecked,
+    AfterViewInit,
+    OnDestroy,
+} from '@angular/core';
 
 @Component({
     selector: 'app-post',
     templateUrl: './post.component.html',
     styleUrls: ['./post.component.css'],
 })
-export class PostComponent implements OnInit {
+export class PostComponent
+    implements
+        OnInit,
+        OnChanges,
+        DoCheck,
+        AfterContentChecked,
+        AfterViewChecked,
+        AfterContentInit,
+        AfterViewInit,
+        OnDestroy
+{
     @Input('img') postImg = '';
     @Output() imgSelected = new EventEmitter<string>();
 
@@ -15,5 +38,33 @@ export class PostComponent implements OnInit {
 
     ngOnInit() {
         console.log('ngOnInit() called', this.postImg);
+    }
+
+    ngOnChanges() {
+        console.log('on changes');
+    }
+
+    ngDoCheck() {
+        console.log('do check');
+    }
+
+    ngAfterContentChecked() {
+        console.log('a-content-checked');
+    }
+
+    ngAfterViewChecked() {
+        console.log('a-view-checked');
+    }
+
+    ngAfterContentInit() {
+        console.log('a-content-init');
+    }
+
+    ngAfterViewInit() {
+        console.log('a-view-init');
+    }
+
+    ngOnDestroy() {
+        console.log('destroy');
     }
 }
