49ab9f8775cb3f44389bb10dd5ed9a8ea6ca90fc
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-05-24 10:22:12 +0200

    #05.62 The ngFor directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,11 @@ export class AppComponent {
     name = 'luis ramirez';
     imgURL =
         'https://i.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0';
+    images = [
+        'https://i.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0',
+        'https://i.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0',
+        'https://i.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0',
+    ];
     currentDate = new Date();
     cost = 2000;
     temperature = 25.3;
